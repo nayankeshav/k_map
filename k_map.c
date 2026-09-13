5c50f66d79293d98a13bcb69031d9fea7ac91cd8
Nayan Keshav <nayankeshav29@gmail.com>
2026-09-13 13:56:56 +0530

    Updating With Python

--- k_map.c
+++ k_map.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-#define max_minterm 16
+#define max_minterm 64
 
 int power(int, int);
 int max_powered_to_2(int);
+int string_int_converter(char *, int);
 
 void arrange_matrix(int x, int y, int main[x][y], int arr[]);
 void render(int x, int y, int main[x][y]);
+void grey_code_generator(int[],int,int);
 
 int main(){
     int min_term_values[max_minterm] = {0};
@@ -31,36 +33,54 @@ int main(){
 
     int max_powered = max_powered_to_2(largest_term_in_minterm+1); // Finding the maximum powered number to 2
 
-    printf("Maxinum value : %d, Powerd by : %d", largest_term_in_minterm, max_powered);
+    printf("Maxinum value : %d, Powerd by : %d \n", largest_term_in_minterm, max_powered);
 
-    int x_matrix, y_matrix; //These Represent x,y Matrix Variables, (Here 0 = One Variable, 1= Two Varibales)
+    int x_matrix, y_matrix, x_matrix_mat, y_matrix_mat; //These Represent x,y Matrix Variables, (Here 0 = One Variable, 1= Two Varibales)
 
     if(max_powered%2 == 0){
         x_matrix = max_powered / 2;
+        x_matrix_mat = x_matrix;
         if(x_matrix == 0){
             x_matrix = 1;
         }
-        y_matrix = x_matrix;
+        y_matrix = max_powered / 2;
+        y_matrix_mat = y_matrix;
+        if(y_matrix == 0){
+            y_matrix = 1;
+        }
     }
     else{
         x_matrix = max_powered / 2;
+        x_matrix_mat = x_matrix;
         if(x_matrix == 0){
             x_matrix = 1;
-            y_matrix = x_matrix;
         }
         else{
-            y_matrix = x_matrix + 1;
+            y_matrix = (max_powered / 2) + 1;
+            y_matrix_mat = y_matrix;
+            if(y_matrix == 0){
+                y_matrix = 1;
+            }
         }
     }
-    x_matrix = x_matrix * 2;
-    y_matrix = y_matrix * 2;
 
-    printf("X  : %d , Y : %d",x_matrix, y_matrix);
-
-    int main_matrix[x_matrix][y_matrix];
+    //Here the Matrix 2^n is split in 2^x_matrix & 2^y_matrix
+    printf("X  : %d , Y : %d",power(2,x_matrix), power(2,y_matrix));
     
-    arrange_matrix(x_matrix, y_matrix, main_matrix, min_term_values);
-    render(x_matrix, y_matrix, main_matrix);
+    //Declaring main matrix
+    int main_matrix[power(2,x_matrix)][power(2,y_matrix)];
+
+    //Creating a the binary row and colume array that corresponds to The Grey Code Rule
+    int binary_row[power(2,x_matrix)],binary_col[power(2,y_matrix)];
+
+    grey_code_generator(binary_row,power(2,x_matrix),++x_matrix);
+
+    for(int i = 0; i < x_matrix; i++){
+        printf("\n row : %d , %d\n",i,binary_row[i]);
+    }
+    
+    arrange_matrix(power(2,x_matrix), power(2,y_matrix), main_matrix, min_term_values);
+    render(power(2,x_matrix), power(2,y_matrix), main_matrix);
 
 }
 
@@ -90,7 +110,9 @@ int max_powered_to_2(int max_number){
 
 void arrange_matrix(int x, int y, int main[x][y], int arr[]){
     printf("\n");
-    for(int i = 0; i<x; i++){
+
+    //Assigning all values of the 2D matrix to 0
+    for(int i = 0; i<x; i++){  
         for(int j = 0; j<y; j++){
             main[i][j] = 0;
         }
@@ -106,3 +128,12 @@ void render(int x, int y, int main[x][y]){
     }
 }
 
+void grey_code_generator(int arr[],int limit, int length){
+    //greycode generated as string
+    
+
+}
+
+int string_int_converter(char *string, int size){
+    //
+}
---
+++ k_map.py
@@ -0,0 +1,77 @@
+
+def highest_power_limit_two_checker(max_value):
+    x = 0
+    while True:
+        if(max_value == 1 or max_value == 0):
+            return 1
+        if(max_value < pow(2,x)):
+            return x
+        x = x + 1
+
+def grey_code(length, bit_limit):
+    result = []
+
+    for i in range(length):
+        curresponding_binary = f"{i:0{bit_limit}b}"
+        grey = [curresponding_binary[0]]
+
+        for j in range(len(curresponding_binary) - 1):
+            xor_bit = int(curresponding_binary[j]) ^ int(curresponding_binary[j + 1])
+            grey.append(str(xor_bit))
+
+        result.append("".join(grey))
+    return result
+
+
+intial_stg_1_list = [] #Intial Stage 1 list containing the min term value
+
+while True:
+    main_input = input("Enter Minterms (enter empty to exit): ")
+    if(main_input == ""):
+        break
+    else:
+        try:
+            intial_stg_1_list.append(int(main_input))
+
+        except Exception as e:
+            print(f"Error Occured : {e}")
+
+#Highest Power Limit (2^n)
+highes_power_of_two_limit = highest_power_limit_two_checker(max(intial_stg_1_list))
+print(f"Highes Power 2^{highes_power_of_two_limit}")
+
+# Next Step Includes creating the table, rows and columns. Well we have to basically create create a shift of the power of 2
+# For this instance we create left rows less than columns for odd numbered situation or else we have equal rows and columns
+
+col = 0
+row = 0
+
+if(highes_power_of_two_limit % 2 == 0):
+    row = highes_power_of_two_limit / 2
+    col = row
+else:
+    row = int(highes_power_of_two_limit / 2)
+    col = row + 1
+
+print(f"The Row And Colums , 2^{row} X 2^{col}")
+
+#declaring the main matrix list
+# NOTICE we have only 2 powered ow and colum vaue so inorder to create table exact 2 raised value
+
+main_matrix_list = []
+for _ in range(pow(2,row)):
+    row_line = [0] * pow(2,col)
+    main_matrix_list.append(row_line)
+
+print(main_matrix_list)
+
+#We also need to know the highet bit length of the row and colum, as be pair location with greycode, we have to reach exact locaion
+row_binary_length = row
+col_binary_length = col
+
+list_of_row_grey_code_seq = grey_code(pow(2,row), row_binary_length)
+print(f"List of Grey Code row : {list_of_row_grey_code_seq}")
+
+list_of_col_grey_code_seq = grey_code(pow(2,col), col_binary_length)
+print(f"List of ")
+
